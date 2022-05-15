caf06db43c62ae36877684bf646f7224f99a24e9
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-05-15 18:43:03 +0200

    Form adding column excersize

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,27 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
+import {useEffect, useState} from 'react';
+
 
 const List = () => {
+    const [columns, setColumns] = useState([
+        {id: 1,title: 'Books', icon: 'book'},
+        {id: 2,title: 'Movies', icon: 'film'},
+        {id: 3,title: 'Games', icon: 'gamepad'},
+    ]);
+
+    useEffect(()=> {
+        setTimeout(() => {
+            setColumns([...columns, {id: 4, title: 'Test column'}]);
+        }, 2000);
+    }, []);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        //setColumns([...columns, {id: ?? , title:??,}]);
+    }
+    const [value, setValue] =useState('aaa');
+
     return (
         <div>
             <header className={styles.header}>
@@ -9,10 +29,14 @@ const List = () => {
             </header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
-                <Column title="Books" icon="book"/>
-                <Column title="Movies" icon="film"/>
-                <Column title="Games" icon="gamepad"/>
+
+                {columns.map(column => <Column  key={column.id} title={column.title} icon={column.icon}/>)}
+                
             </section>
+            <form onSubmit={handleSubmit}>
+                <input type="text"></input>
+                <button>Add column</button>
+            </form>
         </div>
     );
 };
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,7 +1,8 @@
 import styles from './TextInput.module.scss';
 
 const TextInput = (props) => {
-    return (<input className={styles.input} placeholder={props.placeholder} />);
+    return (<input type="text" value={value} onChange={ e=>setValue(e.target.value)} 
+    className={styles.input} placeholder={props.placeholder} />);
 };
 
 export default TextInput;
