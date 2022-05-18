906a9ad51fbc7b059837813eaaa39b20b66d6bee
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-05-18 21:28:25 +0200

    Rendering cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,10 +1,14 @@
 import styles from '../Column/Column.module.scss';
+import Card from '../Card/Card';
 
 const Column = props => {
     console.log(props);
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}/>{props.title}</h2>
+            <ul className={styles.cards}>
+                {props.cards.map(card => <Card key={card.id} title={card.title}/>)}
+            </ul>
         </article>
     );
 };
