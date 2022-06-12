007a4e8cc35d637bdcea542db4bef7a06a55375d
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-06-12 15:23:40 +0200

    useSelector within cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,21 @@
 import styles from '../Column/Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
-    console.log(props);
-    console.log('props.cards: ', props.cards)
+
+    const cards = useSelector(state => state.cards.filter(card =>card.columnId === props.id) );
+    const columns = useSelector(state => state.columns );
+    console.log(cards);
+    
     return (
         <article className={styles.column}>
-            <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>
+            <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + columns.icon} />{columns.title}</h2>
             <ul className={styles.cards}>
-                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+                {cards.map(card => <Card key={card.id} title={card.title} />)}
             </ul>
-            <CardForm columnId={props.columnId} action={props.action} />
+            <CardForm columnId={cards.columnId} />
         </article>
     );
 };
